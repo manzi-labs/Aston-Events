2380c8571b45064f1412efa71a37ff1bb43ba542
manzi <manzirii@aston.ac.uk>
2021-08-15 17:00:33 +0100

    Remove CSRF Protection continued

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -3,7 +3,6 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <!--Bootstrap-->
         <link rel="stylesheet" href="{{ secure_asset("css/app.css") }}">
